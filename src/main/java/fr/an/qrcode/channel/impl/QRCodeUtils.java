f80a8c7443dbd1e22017c5e48f5003ac3a22c57b
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2016-10-02 19:25:00 +0200

    fix

--- src/main/java/fr/an/qrcode/channel/impl/QRCodeUtils.java
+++ src/main/java/fr/an/qrcode/channel/impl/QRCodeUtils.java
@@ -13,15 +13,23 @@ public class QRCodeUtils {
 	    Version version = Version.getVersionForNumber(versionNumber);
 	    int dimForVersion = version.getDimensionForVersion();
 	    for(ErrorCorrectionLevel ecLevel : ErrorCorrectionLevel.values()) {
-	        int maxBytes = com.google.zxing.qrcode.encoder.Encoder.maxBitsWillFit(version, ecLevel);
+	        int maxBytes = qrCodeBitsCapacity(version, ecLevel) / 8;
 	        System.out.println("QRCode v " + versionNumber + " (dim:" + dimForVersion + "), ecLevel:" + ecLevel + " => maxBytes:" + maxBytes);
 	    }
 	}
 	
-	public static int qrCodeBitsCapacity(Version version, ErrorCorrectionLevel errorCorrectionLevel) {
-		return com.google.zxing.qrcode.encoder.Encoder.maxBitsWillFit(version, errorCorrectionLevel);
-	}
-	
+	public static int qrCodeBitsCapacity(Version version, ErrorCorrectionLevel ecLevel) {
+		// partial copy&paste from com.google.zxing.qrcode.encoder.Encoder.willFit() ... but return max value, instead of boolean if value exceed max
+		// In the following comments, we use numbers of Version 7-H.
+		// numBytes = 196
+		int numBytes = version.getTotalCodewords();
+		// getNumECBytes = 130
+		Version.ECBlocks ecBlocks = version.getECBlocksForLevel(ecLevel);
+		int numEcBytes = ecBlocks.getTotalECCodewords();
+		// getNumDataBytes = 196 - 130 = 66
+		int numDataBytes = numBytes - numEcBytes;
+		return numDataBytes;
+	}	
 
     public static Map<DecodeHintType, Object> createDefaultDecoderHints() {
     	Map<DecodeHintType, Object>  qrHints = new HashMap<>();
